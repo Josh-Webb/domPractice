249b1f2bded2b54633b574cd4b2204c51cae05bd
Josh Webb <joshwebb42@gmail.com>
2020-02-28 20:45:25 -0600

    get element types

--- dom4/scripts/main.js
+++ dom4/scripts/main.js
@@ -1,5 +1,18 @@
 function getAttributes()
 {
-    var u = document.getElementById("w3r").href;
-    alert('The value of the href attribute of the link is : '+u)
+    var h = document.getElementById("w3r").href;
+    alert('The value of the href attribute of the link is : '+h);
+
+    var l = document.getElementById("w3r").hreflang;
+    alert('The value of the hreflang attribute of the link is : '+l)
+
+    var r = document.getElementById("w3r").rel;
+    alert('The value of the rel attribute of the link is : '+r)
+
+    var t = document.getElementById("w3r").target;
+    alert('The value of the target attribute of the link is : '+t)
+
+    var a = document.getElementById("w3r").type;
+    alert('The value of the type attribute is : ' +a)
+
 }
